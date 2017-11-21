5168901641548ecf601f992f3b95ea656ed4b1c7
Arturo Amador Paulino <amadorassassin@gmail.com>
2017-11-21 11:17:15 -0600

    trapecios

--- app/js/gauss/gaussController.js
+++ app/js/gauss/gaussController.js
@@ -4,7 +4,16 @@ define(["app","js/gauss/gaussView", "js/gauss/gaussModel"], function(app, gaussV
 		element: '.demo1',
 		event: 'click',
 		handler: demo1,
+	},{
+		element: '.metodo',
+		event: 'click',
+		handler: metodo,
 	}];	
+
+	function metodo(){
+		app.f7.popup('.popup-gauss');
+		console.log("gauss metodo");
+	}
 
 	function demo1(){
 		app.f7.alert('You choose demo 1');
